8bf5eb21bb56ef79eb808cabb9046f157d3713bf
Ethan Caballero <ethancaballero@Ethans-MacBook-Pro.local>
2021-08-09 23:05:07 -0400

    meta_job.sh

---
+++ meta_job.sh
@@ -0,0 +1,19 @@
+sbatch --time=2:30:00 job.sh --wc=0.0001 --u=200 --ib_penalty_activate_epoch=1 --ib_penalty_ramp_over_epoch=200 --ib_penalty=1.0 --bs=64 --method="baseline"
+sbatch --time=2:30:00 job.sh --wc=0.0001 --u=200 --ib_penalty_activate_epoch=1 --ib_penalty_ramp_over_epoch=200 --ib_penalty=1.0 --bs=64 --method="ib_erm"
+sbatch --time=2:30:00 job.sh --wc=0.0001 --u=200 --ib_penalty_activate_epoch=1 --ib_penalty_ramp_over_epoch=200 --ib_penalty=1.0 --bs=64 --method="ib_cirmv1"
+sbatch --time=2:30:00 job.sh --wc=1.0 --u=200 --ib_penalty_activate_epoch=1 --ib_penalty_ramp_over_epoch=200 --ib_penalty=1.0 --bs=64 --method="ib_irmv1"
+
+sbatch --time=2:30:00 job.sh --wc=0.0001 --u=200 --ib_penalty_activate_epoch=1 --ib_penalty_ramp_over_epoch=200 --ib_penalty=1.0 --bs=64 --method="baseline"
+sbatch --time=2:30:00 job.sh --wc=0.0001 --u=200 --ib_penalty_activate_epoch=1 --ib_penalty_ramp_over_epoch=200 --ib_penalty=1.0 --bs=64 --method="ib_erm"
+sbatch --time=2:30:00 job.sh --wc=0.0001 --u=200 --ib_penalty_activate_epoch=1 --ib_penalty_ramp_over_epoch=200 --ib_penalty=1.0 --bs=64 --method="ib_cirmv1"
+sbatch --time=2:30:00 job.sh --wc=1.0 --u=200 --ib_penalty_activate_epoch=1 --ib_penalty_ramp_over_epoch=200 --ib_penalty=1.0 --bs=64 --method="ib_irmv1"
+
+sbatch --time=2:30:00 job.sh --wc=0.0001 --u=200 --ib_penalty_activate_epoch=1 --ib_penalty_ramp_over_epoch=200 --ib_penalty=1.0 --bs=64 --method="baseline"
+sbatch --time=2:30:00 job.sh --wc=0.0001 --u=200 --ib_penalty_activate_epoch=1 --ib_penalty_ramp_over_epoch=200 --ib_penalty=1.0 --bs=64 --method="ib_erm"
+sbatch --time=2:30:00 job.sh --wc=0.0001 --u=200 --ib_penalty_activate_epoch=1 --ib_penalty_ramp_over_epoch=200 --ib_penalty=1.0 --bs=64 --method="ib_cirmv1"
+sbatch --time=2:30:00 job.sh --wc=1.0 --u=200 --ib_penalty_activate_epoch=1 --ib_penalty_ramp_over_epoch=200 --ib_penalty=1.0 --bs=64 --method="ib_irmv1"
+
+sbatch --time=2:30:00 job.sh --wc=0.0001 --u=200 --ib_penalty_activate_epoch=1 --ib_penalty_ramp_over_epoch=200 --ib_penalty=1.0 --bs=64 --method="baseline"
+sbatch --time=2:30:00 job.sh --wc=0.0001 --u=200 --ib_penalty_activate_epoch=1 --ib_penalty_ramp_over_epoch=200 --ib_penalty=1.0 --bs=64 --method="ib_erm"
+sbatch --time=2:30:00 job.sh --wc=0.0001 --u=200 --ib_penalty_activate_epoch=1 --ib_penalty_ramp_over_epoch=200 --ib_penalty=1.0 --bs=64 --method="ib_cirmv1"
+sbatch --time=2:30:00 job.sh --wc=1.0 --u=200 --ib_penalty_activate_epoch=1 --ib_penalty_ramp_over_epoch=200 --ib_penalty=1.0 --bs=64 --method="ib_irmv1"
